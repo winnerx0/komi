280571353ba957c8c53659c9192272e863f7e968
winnerx0 <eragbonikpeya@gmail.com>
2025-06-21 22:59:48 +0100

    update: Add DEL option

--- main.go
+++ main.go
@@ -14,7 +14,6 @@ type Store struct {
 	Value string `json:"value"`
 }
 
-
 func startUp(store map[string]any) {
 	storeBytes, err := os.ReadFile("db/komi.json")
 
@@ -30,7 +29,6 @@ func startUp(store map[string]any) {
 
 	fmt.Println(store)
 
-	
 }
 
 func saveFile(conn net.Conn, key string, value string, store map[string]any) {
@@ -41,16 +39,11 @@ func saveFile(conn net.Conn, key string, value string, store map[string]any) {
 
 	file, err := os.OpenFile("db/komi.json", os.O_CREATE|os.O_WRONLY, 0755)
 
-	defer file.Close()
-
 	if err != nil {
 		log.Fatal("Error creating file ", err)
 	}
 
-	if err != nil {
-		log.Fatal("Error reading db ", err)
-	}
-
+	defer file.Close()
 
 	for k := range store {
 		if k == key {
@@ -61,22 +54,24 @@ func saveFile(conn net.Conn, key string, value string, store map[string]any) {
 	store[key] = value
 
 	bytes, err := json.Marshal(store)
+
 	if err != nil {
 		log.Fatal("Error parsing store ", err)
 	}
 
 	_, err = file.Write(bytes)
 
-	conn.Write([]byte("Set Successfully\n"))
-
 	if err != nil {
 		log.Fatal("Error writing to db ", err)
 	}
+
+	conn.Write([]byte("Set Successfully\n"))
 }
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
+	conn.Write([]byte("Welcome To Komi\n"))
 	store := make(map[string]any)
 
 	startUp(store)
@@ -84,6 +79,7 @@ func handleConnection(conn net.Conn) {
 
 	for {
 		n, err := conn.Read(opt)
+
 		if err != nil {
 			log.Fatal("Error reading from server ", err)
 		}
@@ -109,10 +105,9 @@ func handleConnection(conn net.Conn) {
 
 			saveFile(conn, key, value, store)
 
-
 		case "LIST":
-			storeBytes, err := json.Marshal(store) 
-if err != nil {
+			storeBytes, err := json.Marshal(store)
+			if err != nil {
 				log.Fatal("Error reading db ", err)
 			}
 
@@ -125,15 +120,51 @@ if err != nil {
 
 			key := parts[1]
 
+			found := false
 			for k, v := range store {
 				if k == key {
 					response := fmt.Sprintf("Key: %s\nValue: %s\n", k, v)
 					conn.Write([]byte(response))
-					return
-
+					found = true
+					break
 				}
+
 			}
-			conn.Write([]byte("No value stored in database for " + key + "\n"))
+			if !found {
+				conn.Write([]byte("No value stored in database for " + key + "\n"))
+			}
+		case "DEL":
+
+			if len(parts) < 2 {
+				conn.Write([]byte("No key entered"))
+				continue
+			}
+			key := parts[1]
+
+			if store[key] == nil {
+				conn.Write([]byte("Data not found"))
+			}
+
+			delete(store, key)
+
+			file, err := os.OpenFile("db/komi.json", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0755)
+
+			defer file.Close()
+
+			if err != nil {
+				log.Fatal("Error opening file", err)
+			}
+
+			storeBytes, err := json.Marshal(store)
+
+			if err != nil {
+				log.Fatal("Error dencoding store ", err)
+			}
+
+			file.Write(storeBytes)
+
+			fmt.Println(store)
+			conn.Write([]byte("Deleted Successfully\n"))
 		default:
 			conn.Write([]byte("Invalid option\n"))
 		}
@@ -141,17 +172,16 @@ if err != nil {
 	}
 }
 
-
 func main() {
 	l, err := net.Listen("tcp", ":3005")
-
-	defer l.Close()
 
 	fmt.Println("Listening to server 3005")
 
 	if err != nil {
 		log.Fatal("Error starting connection ", err)
 	}
+
+	defer l.Close()
 
 	for {
 
